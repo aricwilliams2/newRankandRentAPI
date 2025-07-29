197e28c18d5b07448f0aa368bf77b0fd9cd34942
aricwilliams2 <aricwilliamst2@gmail.com>
2025-07-29 16:17:54 -0400

    Updated leadRoutes.js

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -7,7 +7,7 @@ const { validateLead } = require('../middleware/validation');
 // Lead routes
 router.get('/leads', authenticate, LeadController.index);
 router.get('/leads/:id', authenticate, LeadController.show);
-router.post('/leads', authenticate, validateLead('store'), LeadController.store);
+router.post('/leads', validateLead('store'), LeadController.store);
 router.put('/leads/:id', authenticate, validateLead('update'), LeadController.update);
 router.delete('/leads/:id', authenticate, LeadController.destroy);
 
