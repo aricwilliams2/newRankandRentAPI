310d395ee8a6a90466c60237178afd94d582e804
Aric Williams <aricwilliamst@gmail.com>
2025-08-20 16:36:59 -0400

    update letst see

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
+  'https://rankandrenttool.com',
   process.env.FRONTEND_URL,          // e.g. https://rankandrenttool.com
 ].filter(Boolean);
 
